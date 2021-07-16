78c4d2b5758fe63ffc5c042a6cdbe19bc9e2c21f
Cooper Gillan <cooper.gillan@gmail.com>
2021-07-16 22:08:11 +0000

    Add import functionality

---
+++ examples/import/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    hashicups = {
+      version = "0.2"
+      source  = "hashicorp.com/edu/hashicups"
+    }
+  }
+}
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+}
+
+resource "hashicups_order" "sample" {}
+resource "hashicups_order" "cooper" {}
+
+output "sample_order" {
+  value = hashicups_order.sample
+}
